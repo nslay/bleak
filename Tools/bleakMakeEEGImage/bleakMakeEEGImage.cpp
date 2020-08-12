3fd8e13bfd2fa61085650a61279cd9f309f9b2a6
nslay <nslay@users.noreply.github.com>
2020-08-11 23:13:24 -0400

    Add ability to exclude some EEG channels.

--- Tools/bleakMakeEEGImage/bleakMakeEEGImage.cpp
+++ Tools/bleakMakeEEGImage/bleakMakeEEGImage.cpp
@@ -28,26 +28,66 @@
 #include <cstring>
 #include <iostream>
 #include <string>
+#include <utility>
 #include <fstream>
+#include <unordered_map>
+#include <unordered_set>
 #include "Common.h"
+#include "bsdgetopt.h"
 
 // ITK stuff
 #include "itkImage.h"
 #include "itkMetaImageIOFactory.h"
 #include "itkNiftiImageIOFactory.h"
 #include "itkImageFileWriter.h"
+#include "itkMetaDataDictionary.h"
+#include "itkMetaDataObject.h"
 
 // This tool prepares data from: https://kdd.ics.uci.edu/databases/eeg/eeg.html
 // It constructs 2D float images with Y axis = channel and X axis = time
 // This is probably a good data set for 1D convolution
 
+namespace {
+
+const char * const g_a_cChannelNames[] = {
+  "FP1", "FP2", "F7", "F8",
+  "AF1", "AF2", "FZ", "F4",
+  "F3", "FC6", "FC5", "FC2",
+  "FC1", "T8", "T7", "CZ",
+  "C3", "C4", "CP5", "CP6",
+  "CP1", "CP2", "P3", "P4",
+  "PZ", "P8", "P7", "PO2",
+  "PO1", "O2", "O1", "X",
+  "AF7", "AF8", "F5", "F6",
+  "FT7", "FT8", "FPZ", "FC4",
+  "FC3", "C6", "C5", "F2",
+  "F1", "TP8", "TP7", "AFZ",
+  "CP3", "CP4", "P5", "P6",
+  "C1", "C2", "PO7", "PO8",
+  "FCZ", "POZ", "OZ", "P2",
+  "P1", "CPZ", "nd", "Y",
+  nullptr
+};
+
+std::unordered_map<std::string, int> g_mChannelMappings;
+std::unordered_set<std::string> g_sExcludedChannels;
+
+} // end anonymous namespace
+
 void Usage(const char *p_cArg0) {
-  std::cerr << "Usage: " << p_cArg0 << " eegCaseFile eegOutputImage.mha" << std::endl;
+  std::cerr << "Usage: " << p_cArg0 << " [-h] [-e excludedChannelName,excludedChannelName2,...] eegReadFile eegOutputImage.mha" << std::endl;
   exit(1);
 }
 
+std::ostream & WarnOnce(const std::string &strChannelName) {
+  static std::unordered_set<std::string> sWarnedChannels;
+  static std::ostream blackhole(nullptr);
+
+  return sWarnedChannels.emplace(strChannelName).second ? std::cerr : blackhole;
+}
+
 void RegisterITKFactories();
-int GetChannelIndex(const char *p_cChannelName);
+int GetChannelIndex(const std::string &strChannelName);
 
 // From AlignVolumes
 template<typename PixelType, unsigned int Dimension>
@@ -60,11 +100,32 @@ int main(int argc, char **argv) {
 
   RegisterITKFactories();
 
-  if (argc != 3)
+  int c = 0;
+  while ((c = getopt(argc, argv, "e:h")) != -1) {
+    switch (c) {
+    case 'e':
+      {
+        std::vector<std::string> vTokens = bleak::SplitString<std::string>(optarg, ",");
+        g_sExcludedChannels.insert(vTokens.begin(), vTokens.end());
+      }
+      break;
+    case 'h':
+      Usage(p_cArg0);
+      break;
+    case '?':
+    default:
+      Usage(p_cArg0);
+    }
+  }
+
+  argc -= optind;
+  argv += optind;
+
+  if (argc != 2)
     Usage(p_cArg0);
 
-  const char *p_cEegFile = argv[1];
-  const char *p_cEegImage = argv[2];
+  const char *p_cEegFile = argv[0];
+  const char *p_cEegImage = argv[1];
 
   itk::Image<float, 2>::Pointer p_clImage = ReadEeg(p_cEegFile);
 
@@ -88,36 +149,22 @@ void RegisterITKFactories() {
   itk::NiftiImageIOFactory::RegisterOneFactory();
 }
 
-int GetChannelIndex(const char *p_cChannelName) {
-  static const char * const a_cChannelNames[] = {
-    "FP1", "FP2", "F7", "F8",
-    "AF1", "AF2", "FZ", "F4",
-    "F3", "FC6", "FC5", "FC2",
-    "FC1", "T8", "T7", "CZ",
-    "C3", "C4", "CP5", "CP6",
-    "CP1", "CP2", "P3", "P4",
-    "PZ", "P8", "P7", "PO2",
-    "PO1", "O2", "O1", "X",
-    "AF7", "AF8", "F5", "F6",
-    "FT7", "FT8", "FPZ", "FC4",
-    "FC3", "C6", "C5", "F2",
-    "F1", "TP8", "TP7", "AFZ",
-    "CP3", "CP4", "P5", "P6",
-    "C1", "C2", "PO7", "PO8",
-    "FCZ", "POZ", "OZ", "P2",
-    "P1", "CPZ", "nd", "Y",
-    nullptr
-  };
+int GetChannelIndex(const std::string &strChannelName) {
+  if (g_mChannelMappings.empty()) {
+    // Construct mapping
+    for (int i = 0; g_a_cChannelNames[i] != nullptr; ++i) {
+      std::string strChannelName = g_a_cChannelNames[i];
 
-  if (p_cChannelName == nullptr)
-    return -1;
-
-  for (int iChannelIndex = 0; a_cChannelNames[iChannelIndex] != nullptr; ++iChannelIndex) {
-    if (strcmp(p_cChannelName, a_cChannelNames[iChannelIndex]) == 0)
-      return iChannelIndex;
+      if (g_sExcludedChannels.find(strChannelName) == g_sExcludedChannels.end()) {
+        const int iChannelIndex = (int)g_mChannelMappings.size();
+        g_mChannelMappings.emplace(std::move(strChannelName), iChannelIndex);
+      }
+    }
   }
 
-  return -1;
+  auto itr = g_mChannelMappings.find(strChannelName);
+
+  return itr != g_mChannelMappings.end() ? itr->second : -1;
 }
 
 template<typename PixelType, unsigned int Dimension>
@@ -143,9 +190,11 @@ bool SaveImg(const itk::Image<PixelType, Dimension> *p_clImage, const std::strin
 }
 
 itk::Image<float, 2>::Pointer ReadEeg(const char *p_cEegFile) {
+  (void)GetChannelIndex("FP1"); // Forcibly build g_mChannelMappings
+
   typedef itk::Image<float, 2> ImageType;
 
-  if (p_cEegFile == nullptr)
+  if (p_cEegFile == nullptr || g_mChannelMappings.empty())
     return nullptr;
 
   std::ifstream eegStream(p_cEegFile);
@@ -157,7 +206,7 @@ itk::Image<float, 2>::Pointer ReadEeg(const char *p_cEegFile) {
 
   itk::Size<2> clSize;
   clSize[0] = 256;
-  clSize[1] = 64;
+  clSize[1] = g_mChannelMappings.size();
 
   ImageType::Pointer p_clImage = ImageType::New();
   ImageType::SpacingType clSpacing;
@@ -198,11 +247,11 @@ itk::Image<float, 2>::Pointer ReadEeg(const char *p_cEegFile) {
     if (vTokens.size() != 4)
       continue;
 
-    const int iChannelNumber = GetChannelIndex(vTokens[1].c_str());
+    const int iChannelNumber = GetChannelIndex(vTokens[1]);
 
     if (iChannelNumber < 0) {
-      std::cerr << "Error: Could not determine channel index for channel name '" << vTokens[1] << "'." << std::endl;  
-      return nullptr;
+      WarnOnce(vTokens[1]) << "Warning: Could not determine channel index for channel name '" << vTokens[1] << "'." << std::endl;  
+      continue;
     }
 
     int iTimeIndex = 0;
@@ -232,6 +281,30 @@ itk::Image<float, 2>::Pointer ReadEeg(const char *p_cEegFile) {
 
     p_clImage->SetPixel(clIndex, fVoltage);
   }
+
+  // Embed channel names to metadata
+  {
+    itk::MetaDataDictionary &clMetaData = p_clImage->GetMetaDataDictionary();
+
+    std::vector<std::string> vChannelNames(g_mChannelMappings.size());
+
+    for (const auto &stPair : g_mChannelMappings)
+      vChannelNames[stPair.second] = stPair.first;
+
+    std::string strChannelNames = vChannelNames[0];
+    strChannelNames += "=0";
+
+    for (size_t i = 1; i < vChannelNames.size(); ++i) {
+      strChannelNames += ' ';
+      strChannelNames += vChannelNames[i];
+      strChannelNames += '=';
+      strChannelNames += std::to_string(i);
+    }
+
+    itk::EncapsulateMetaData(clMetaData, "channelNames", strChannelNames);
+  }
+
+
 
   return p_clImage;
 }
